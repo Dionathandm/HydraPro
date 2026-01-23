b7669cb8a34fa027f552a0b909fab3a213334ada
Dionathandm <plugplaysong@gmail.com>
2026-01-23 01:50:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,37 @@
-const CACHE_NAME = "hydra-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js"
+const CACHE_NAME = "hydrapro-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://i.ibb.co/0VBBMfrZ/lv-0-20260119011237.png",
+  "https://i.ibb.co/r22ZV0kv/lv-0-20260116051448.png",
+  "https://i.ibb.co/60KP16PJ/lv-0-20260117003207.png"
 ];
 
+// Instala e adiciona ao cache
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Ativa o Service Worker
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => {
+        if(key !== CACHE_NAME) return caches.delete(key);
+      }))
     )
   );
-  self.clients.claim();
 });
 
+// Intercepta requisições e entrega do cache quando offline
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
